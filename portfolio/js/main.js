0f2d529a01b13feeddbf8524ec7a3c70c5cfed20
MdShameem-eng <72496474+MdShameem-eng@users.noreply.github.com>
2020-10-07 15:22:51 +0530

    Add files via upload

---
+++ portfolio/js/main.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function () {
+	
+	var $logo 	= $('#logo');
+
+    if (location.href.indexOf("#") != -1) {
+        $logo.show();
+    } 
+	$('.menu .tabs a').click(function() {
+	  $logo.fadeIn('slow');
+	}); 
+	$('.tab-profile').click(function() {
+	  $logo.fadeOut('slow');
+	});	
+
+})
